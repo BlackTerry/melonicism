a48e6ea00f9d9b34fb1cb001135439bf4dea0c1f
Terry <thefirstterry@gmail.com>
2020-04-16 19:45:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './melon.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome To The Church of Melonicism.</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <i>
+          <p>
+            At the church of Melonicism we worship the one true Goddess, Melonis.<br></br> 
+            She is a thicc, juicy, curved celestial being.<br></br>Mother of all, Hydrator of all.<br></br>
+            Hydrate thyself with her juices and ye shall live forever.<br></br>
+            Deny her existance and ye shall be thirsty thots and simps for all eternity.
+          </p>
+        </i>
+      </header>
+    </div>
+  );
+}
+
+export default App;
